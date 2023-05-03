eb3abdef572631fdf87e0e2ec8b10fb9c0eb8689
mustaphaamindh@gmail.com <amin29199@gmail.com>
2023-05-03 15:48:31 +0100

    added redux store, custom Material Ui theme and dark mode toggle

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,24 @@
+import {createTheme} from "@mui/material/styles"
+import { useMemo } from "react"
+import { themeSettings } from "./theme"
+import {ThemeProvider, CssBaseline} from '@mui/material'
+import { useDispatch, useSelector } from "react-redux"
+import { toggleTheme } from "./store/reducers/themeSlice"
+
 
 function App() {
 
+
+  const dispatch = useDispatch();
+  const darkMode = useSelector((state:any) => state.theme.darkMode);
+  const theme = useMemo(()=> createTheme(themeSettings(darkMode? "dark": "light")), [darkMode])
+
   return (
     <>
-
+<ThemeProvider theme={theme}>
+<CssBaseline/>
+<button onClick={()=>dispatch(toggleTheme())}>toggle</button>
+</ThemeProvider>
     </>
   )
 }
---
+++ client/src/pages/landing-page/LandingPage.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from './Header'
+import Footer from './Footer'
+
+function LandingPage() {
+  return (
+    <div>
+      <Header/>
+
+      <div> landing page content</div>
+
+      <Footer/>
+
+    </div>
+  )
+}
+
+export default LandingPage
